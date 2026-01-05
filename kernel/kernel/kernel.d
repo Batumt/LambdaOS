71565af1a72c49e6e1b5e06f4dc57a828439c75a
Batumt <batumyusuf09@gmail.com>
2026-01-05 03:21:55 +0300

    v0.5.0: GRUB boot, libc and tty/vga structure working

---
+++ kernel/kernel/kernel.d
@@ -0,0 +1,5 @@
+kernel/kernel.o: kernel/kernel.c \
+ /home/batum/Desktop/LambdaOS/sysroot/usr/include/stdio.h \
+ /home/batum/Desktop/LambdaOS/sysroot/usr/include/sys/cdefs.h \
+ include/kernel/tty.h \
+ /usr/local/cross/lib/gcc/i686-elf/14.2.0/include/stddef.h
