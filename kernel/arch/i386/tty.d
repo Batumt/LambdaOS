7ef65481d083e9a0972c3c63112d89d598204923
Batumt <batumyusuf09@gmail.com>
2026-01-26 23:05:09 +0300

    LambdaOS v0.5

--- kernel/arch/i386/tty.d
+++
@@ -1,8 +0,0 @@
-arch/i386/tty.o: arch/i386/tty.c \
- /usr/local/cross/lib/gcc/i686-elf/14.2.0/include/stdbool.h \
- /usr/local/cross/lib/gcc/i686-elf/14.2.0/include/stddef.h \
- /usr/local/cross/lib/gcc/i686-elf/14.2.0/include/stdint.h \
- /usr/local/cross/lib/gcc/i686-elf/14.2.0/include/stdint-gcc.h \
- /home/batum/Desktop/LambdaOS/sysroot/usr/include/string.h \
- /home/batum/Desktop/LambdaOS/sysroot/usr/include/sys/cdefs.h \
- include/kernel/tty.h arch/i386/vga.h
--- kernel/kernel/kernel.d
+++
@@ -1,5 +0,0 @@
-kernel/kernel.o: kernel/kernel.c \
- /home/batum/Desktop/LambdaOS/sysroot/usr/include/stdio.h \
- /home/batum/Desktop/LambdaOS/sysroot/usr/include/sys/cdefs.h \
- include/kernel/tty.h \
- /usr/local/cross/lib/gcc/i686-elf/14.2.0/include/stddef.h
